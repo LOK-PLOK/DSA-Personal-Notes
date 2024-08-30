28d5ff56469d51055e3d7f459086e0d6b18f3277
PaulFranceM.Detablan <157859793+LOK-PLOK@users.noreply.github.com>
2024-08-30 17:54:09 +0800

    gitignore in the root directory

--- 2.Stack/Test Area/header/LinkedList.h
+++ 2.Stack/Test Area/header/LinkedList.h
@@ -8,14 +8,14 @@
 typedef struct node{
     int data;
     struct node* next;
-}Stack;
+}*Stack;
 
-void init(Stack** A){
+void init(Stack* A){
     (*A) = NULL;
 }
 
-void push(Stack** A, int elem){
-    Stack* temp = (Stack*)malloc(sizeof(Stack));
+void push(Stack* A, int elem){
+    Stack temp = (Stack)malloc(sizeof(struct node));
     if(temp != NULL){
        temp->data = elem;
        temp->next = *A;
@@ -23,9 +23,9 @@ void push(Stack** A, int elem){
     }
 }
 
-void pop(Stack** A){
+void pop(Stack* A){
     if(*A != NULL){
-        Stack* temp = *A;
+        Stack temp = *A;
         *A = temp->next;
         free(temp);
     }else{
@@ -33,14 +33,14 @@ void pop(Stack** A){
     }
 }
 
-int top(Stack* A){
+int top(Stack A){
     return A->data;
 }
 
-void display(Stack* A){
+void display(Stack A){
     printf("\nStack: ");
     if(A != NULL){
-        Stack* trav;
+        Stack trav;
         for(trav = A; trav != NULL; trav = trav->next){
             printf("%d ",trav->data);
         }
--- 2.Stack/Test Area/main.c
+++ 2.Stack/Test Area/main.c
@@ -1,26 +1,7 @@
-
-/*
-    Learnings:
-    header file: array.h
-    ---> has different variable declaration basing from what version
-    example:
-    version 1: Stack S;
-    version 2: Stack* S;  you have to malloc stack
-    version 3: Stack S;
-    version 4: Stack* S; you have to malloc stack and dynamic array
-
-    header file: LinkedList.h
-    ---> following the rules of how a linked list works, the declaration should be a pointer variable
-    example: Stack* S
-
- */
-
-
-// #include "./header/arrayV1.h"
-// #include "./header/arrayV2.h"
+// #include "./header/array.h"
 #include "./header/LinkedList.h"
 int main(){
-    Stack* S;
+    Stack S;
     
     system("cls");
     
