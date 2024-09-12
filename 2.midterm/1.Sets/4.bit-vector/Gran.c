7aca6538c93d87f68521d6209ce706d3b57028ad
PaulFranceM.Detablan <157859793+LOK-PLOK@users.noreply.github.com>
2024-09-12 12:57:00 +0800

    gran act: incomplete

--- 2.midterm/1.Sets/4.bit-vector/Gran.c
+++ 2.midterm/1.Sets/4.bit-vector/Gran.c
@@ -8,10 +8,11 @@ typedef struct {
     int max;
 }Set;
 
-typedef char Set;
+// typedef char Set;
 
 Set createSet(int max);
 void initSet(Set* S, int max);
+
 bool addElem(Set* S, int elem);
 bool removeElem(Set* S, int elem);
 void displaySet(Set S);
@@ -23,6 +24,73 @@ Set Intersect(Set A, Set B);
 Set DifferenceSet(Set A, Set B);
 Set symmetricDifference(Set A, Set B);
 
+int main(){
+    Set A = createSet(5);
+    Set B = createSet(10);
+    
+    printf("Set A: ");
+    displaySet(A);
+    printf("Set B: ");
+    displaySet(B);
+
+    addElem(&A, 0);
+    addElem(&A, 2);
+    addElem(&A, 4);
+    addElem(&B, 0);
+    addElem(&B, 2);
+    addElem(&B, 4);
+    addElem(&B, 3);
+    addElem(&B, 1);
+
+    printf("\nSet A: ");
+    displaySet(A);
+    printf("Set B: ");
+    displaySet(B);
+
+}
+
+Set createSet(int max){
+    Set temp;
+    temp.max = max;
+    temp.count = 0;
+    temp.elem = (int*)calloc(sizeof(int),temp.max);
+    return temp;
+}
+
+void initSet(Set* S, int max){
+    S->max = max;
+    S->count = 0;
+    S->elem = (int*)calloc(sizeof(int),S->max);
+}
+
+void displaySet(Set S){
+    int i;
+    for(i = 0; i<S.max;i++){
+        printf("%d ",S.elem[i]);
+    }
+    printf("\n");
+}
+
+bool addElem(Set* S, int elem){
+    if(S->elem[elem] == 0){
+        S->elem[elem] =1;
+        S->count++;
+        return true;
+    }
+
+    return false;
+}
+
+bool removeElem(Set* S, int elem){
+    if(S->elem[elem] == 1){
+        S->elem[elem] = 0;
+        S->count--;
+        return true;
+    }
+    return false;
+}
+
+bool
 
 /**
  * 
