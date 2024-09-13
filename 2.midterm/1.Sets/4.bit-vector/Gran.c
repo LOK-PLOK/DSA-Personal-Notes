09d2a9189a60ef4253e3a6be479bba84d4713f55
PaulFranceM.Detablan <157859793+LOK-PLOK@users.noreply.github.com>
2024-09-13 15:48:37 +0800

    Gran Activity: Sure Complete

--- 2.midterm/1.Sets/4.bit-vector/Gran.c
+++ 2.midterm/1.Sets/4.bit-vector/Gran.c
@@ -24,146 +24,145 @@ Set Intersect(Set A, Set B);
 Set DifferenceSet(Set A, Set B);
 Set symmetricDifference(Set A, Set B);
 
-int main() {
-    Set A = createSet(6);
+int main(){
+    system("cls");
+    Set A = createSet(7);
     Set B = createSet(10);
-    
-    // Adding elements to A
+
+    //Elements of A
     addElem(&A, 0);
     addElem(&A, 2);
     addElem(&A, 4);
     addElem(&A, 5);
-    
-    // Adding elements to B
+    addElem(&A, 6);
+
+    //Elements of B
     addElem(&B, 0);
     addElem(&B, 1);
     addElem(&B, 2);
-    addElem(&B, 3);
     addElem(&B, 4);
     addElem(&B, 7);
     addElem(&B, 9);
-    
-    // Display sets
+
+    //Print both Sets
     printf("Set A: ");
     displaySet(A);
     printf("Set B: ");
     displaySet(B);
-    
-    // Check if A is a subset of B
-    if (isSubset(A, B)) {
+
+    //Check if A is a subset of B
+    if(isSubset(A, B)){
         printf("\nA is a subset of B\n");
-    } else {
+    }else{
         printf("\nA is not a subset of B\n");
     }
-    
-    // Check if B is a subset of A
-    if (isSubset(B, A)) {
+
+    //Check if B is a subset of A
+    if(isSubset(B, A)){
         printf("B is a subset of A\n");
-    } else {
+    }else{
         printf("B is not a subset of A\n");
     }
-    
-    // Union of A and B
-    Set C = UnionSet(A, B);
+
+    //Union of A and B
+    Set C = UnionSet(A,B);
     printf("\nUnion of A and B: ");
     displaySet(C);
 
-    // Intersect of A and B
-    Set D = Intersect(A, B);
+    //Intersect of A and B
+    Set D = Intersect(A,B);
     printf("Intersect of A and B: ");
     displaySet(D);
 
-    Set E = DifferenceSet(A, B);
+    //Difference of A and B
+    Set E = DifferenceSet(A,B);
     printf("Difference of A and B: ");
     displaySet(E);
 
-    Set F = symmetricDifference(A, B);
-    printf("symmetricDifference of A and B: ");
+    //Symmetric Difference of A and B
+    Set F = symmetricDifference(A,B);
+    printf("Symmetric Difference of A and B: ");
     displaySet(F);
 
-    // Add and remove elements from set A
-    addElem(&A, 1);  // Add element to A
+    //Adding 1 to Set A if it does not have 1
+    addElem(&A, 1);  
     printf("\nAfter adding 1 to A: ");
     displaySet(A);
     
-    removeElem(&A, 0);  // Remove element from A
+    //remove 1 to Set A if its 1
+    removeElem(&A, 0); 
     printf("After removing 0 from A: ");
     displaySet(A);
 
-    // Check if an element is present in a set
+    //Checking if 2 is an element of Set A
     if (isElem(A, 2)) {
         printf("\nElement 2 is in A\n");
     } else {
         printf("Element 2 is not in A\n");
     }
 
-    // Clean up dynamic memory
+    //Cleaning of Dynamic Arrays
     free(A.elem);
     free(B.elem);
     free(C.elem);
     free(D.elem);
     free(E.elem);
     free(F.elem);
-    
+
     return 0;
 }
 
 Set createSet(int max){
     Set temp;
-    temp.max = max;
     temp.count = 0;
+    temp.max = max;
     temp.elem = (int*)calloc(sizeof(int),temp.max);
     return temp;
 }
 
-void initSet(Set* S, int max){
-    S->max = max;
-    S->count = 0;
-    S->elem = (int*)calloc(sizeof(int),S->max);
+bool addElem(Set* S, int index){
+    if(S->elem[index] == 0){
+        S->elem[index] = 1;
+        S->count++;
+        return 1;
+    }
+    return 0;
 }
 
 void displaySet(Set S){
     int i;
-    for(i = 0; i<S.max;i++){
-        printf("%d ",S.elem[i]);
+    for(i = 0; i<S.max; i++){
+        printf("%d ",(S.elem[i] == 1)? 1:0);
     }
     printf("\n");
 }
 
-bool addElem(Set* S, int elem){
-    if(S->elem[elem] == 0){
-        S->elem[elem] =1;
-        S->count++;
-        return true;
-    }
-
-    return false;
-}
-
-bool removeElem(Set* S, int elem){
-    if(S->elem[elem] == 1){
-        S->elem[elem] = 0;
+bool removeElem(Set* S, int index){
+    if(S->elem[index] == 1){
+        S->elem[index] = 0;
         S->count--;
-        return true;
+        return 1;
     }
-    return false;
+    return 0;
 }
 
-bool isElem(Set S, int elem){
-    return (S.elem[elem] == 1)? 1:0;
+bool isElem(Set S, int index){
+    if(S.elem[index] == 1){
+        return 1;
+    }
+    return 0;
 }
 
-bool isSubset(Set A, Set B) {
-    int minMax = A.max;
-
-    for (int i = 0; i < minMax; i++) {
-        if (A.elem[i] == 1 && B.elem[i] == 0) {
-            return false;
+bool isSubset(Set A, Set B){
+    int i;
+    for(i=0; i<A.max; i++){
+        if(A.elem[i] == 1 && B.elem[i] == 0){
+            return 0;
         }
     }
-    return true;
-}
 
+    return 1;
+}
 
 Set UnionSet(Set A, Set B){
     Set big = (A.max > B.max)? A:B;
@@ -171,8 +170,8 @@ Set UnionSet(Set A, Set B){
     Set temp = createSet(big.max);
 
     int i;
-    for(i=0;i<small.max; i++){
-        temp.elem[i] = small.elem[i] | big.elem[i];
+    for(i=0;i<small.max;i++){
+        temp.elem[i] = big.elem[i] | small.elem[i];
     }
 
     while(i<big.max){
@@ -181,7 +180,6 @@ Set UnionSet(Set A, Set B){
     }
 
     return temp;
-
 }
 
 Set Intersect(Set A, Set B){
@@ -190,22 +188,8 @@ Set Intersect(Set A, Set B){
     Set temp = createSet(A.max);
 
     int i;
-    for(i=0;i<small.max; i++){
-        temp.elem[i] = small.elem[i] & big.elem[i];
-    }
-
-    
-    return temp;
-}
-
-Set DifferenceSet(Set A, Set B){
-    Set big = (A.max > B.max)? A:B;
-    Set small = (A.max > B.max)? B:A;
-    Set temp = createSet(A.max);
-
-    int i;
-    for(i=0;i<small.max; i++){
-        temp.elem[i] = small.elem[i] & ~ big.elem[i];
+    for(i=0;i<small.max;i++){
+        temp.elem[i] = big.elem[i] & small.elem[i];
     }
 
     if(temp.max == big.max){
@@ -218,12 +202,38 @@ Set DifferenceSet(Set A, Set B){
     return temp;
 }
 
-Set symmetricDifference(Set A, Set B){
-    int big  = (A.max > B.max)? A.max:B.max;
-    Set temp = createSet(big);
-    Set first = (A,B);
-    Set second = (B,A);
-    temp = UnionSet(first,second);
+Set DifferenceSet(Set A, Set B){
+    Set big = (A.max > B.max)? A:B;
+    Set small = (A.max > B.max)? B:A;
+    Set temp = createSet(A.max);
+
+    int i;
+    for(i=0;i<small.max;i++){
+        temp.elem[i] = small.elem[i] & ~big.elem[i];
+    }
+
+    if(temp.max == big.max){
+        while(i<big.max){
+            temp.elem[i] = big.elem[i];
+            i++;
+        }
+    }
+
+    return temp;
+}
+
+
+Set symmetricDifference(Set A, Set B) {
+    int size = (A.max > B.max) ? A.max : B.max;
+    Set temp = createSet(size);
+
+    Set diffAB = DifferenceSet(A, B);
+    Set diffBA = DifferenceSet(B, A);
+
+    temp = UnionSet(diffAB, diffBA);
+
+    free(diffAB.elem);
+    free(diffBA.elem);
 
     return temp;
 }
