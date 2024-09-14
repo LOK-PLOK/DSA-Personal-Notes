03bae7d192edfb64702b236e379bb8e5f90b8846
PaulFranceM.Detablan <157859793+LOK-PLOK@users.noreply.github.com>
2024-09-14 11:13:06 +0800

    stack review

---
+++ 1.pre-mid/2.Stack/1.array/review.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define MAX
+
+/**
+ * Stack operations
+ * Push() insert an element at the top of the stack
+ * Pop() delete an elemen at the top of the stack
+ * Top() return the top element of the stack // different for ADT 
+ * Size() return the size of the stack
+ * isEmpty() returns 1 if true and 0 if not
+ * isFull() returns 1 if true and 0 if not
+ */
+
