6d6a7052d12f585878685b2d17ddc0c3e8bec0d3
PaulFranceM.Detablan <157859793+LOK-PLOK@users.noreply.github.com>
2024-09-14 14:20:20 +0800

    Stack array implementation review

--- 1.pre-mid/2.Stack/1.array/review.c
+++ 1.pre-mid/2.Stack/1.array/review.c
@@ -1,5 +1,100 @@
 #include <stdio.h>
-#define MAX
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX 10
+
+typedef struct {
+    char elem[MAX];
+    int top;
+}Stack;
+
+Stack initStack();
+void displayStack(Stack A);
+char topElem(Stack A);
+bool isEmpty(Stack A);
+bool isFull(Stack A);
+
+void Push(Stack* A, char elem);
+void Pop(Stack* A);
+
+int main(){
+    system("cls");
+    Stack S = initStack();
+
+
+    printf("\ninitStack()\n");
+    printf("Stack S: ");
+    displayStack(S);
+
+    printf("\nPush(A,B,C,D,E)\n");
+    printf("Stack S: ");
+    Push(&S,'A');
+    Push(&S,'B');
+    Push(&S,'C');
+    Push(&S,'D');
+    Push(&S,'E');
+    displayStack(S);
+
+    printf("\nTop element: %c\n",topElem(S));
+
+    printf("\nPop() 2 times\n");
+    Pop(&S);
+    Pop(&S);
+    displayStack(S);
+}
+
+Stack initStack(){
+    Stack temp;
+    temp.top = -1;
+    return temp;
+}
+
+void Push(Stack* A, char elem){
+    if(isFull(*A) != 1){
+        A->elem[++A->top] = elem;
+    } 
+}
+
+void Pop(Stack* A){
+    if(isEmpty(*A) != 1){
+        A->top--;
+    }
+}
+
+char topElem(Stack A){
+    return (A.top != -1)? A.elem[A.top]: '-';
+}
+
+bool isEmpty(Stack A){
+    return (A.top == -1)? 1:0;
+}
+bool isFull(Stack A){
+    return (A.top == MAX -1)? 1:0;
+}
+
+void displayStack(Stack A){
+    if(!isEmpty(A)){
+        Stack temp = initStack();
+
+        while(A.top != -1){
+            printf("%c ",A.elem[A.top]);
+            Push(&temp, A.elem[A.top]);
+            Pop(&A);
+        }
+
+        while(temp.top != -1){
+            Push(&A, temp.elem[temp.top]);
+            Pop(&temp);
+        }
+
+        printf("\n");
+    }else{
+        printf("Empty\n");
+    }
+    
+}
+
+
 
 /**
  * Stack operations
@@ -10,4 +105,3 @@
  * isEmpty() returns 1 if true and 0 if not
  * isFull() returns 1 if true and 0 if not
  */
-
