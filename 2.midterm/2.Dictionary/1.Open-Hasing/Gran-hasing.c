9b06afc8c9d895f3f8af7c7a4c2d54ac1a4aced0
PaulFranceM.Detablan <157859793+LOK-PLOK@users.noreply.github.com>
2024-09-30 18:19:22 +0800

    feat: get hash, initDic,createStud,insert,delete and display functions

--- 2.midterm/2.Dictionary/1.Open-Hasing/Gran-hasing.c
+++ 2.midterm/2.Dictionary/1.Open-Hasing/Gran-hasing.c
@@ -66,3 +66,78 @@ int main(){
 }
 
 // create function definitions here
+void initDictionary(HashTable *ht){
+    ht->count =0;
+    int i;
+    for(i=0;  i<MAX; i++){
+        ht->elems[i] = NULL;
+    }
+}
+
+int getHash(Student s){
+    int nameSum = s.studName[0] + s.studName[1] + s.studName[2];
+    int courseSum = 0;
+    for(int i=0; s.program[i] != '\0'; i++){
+        courseSum += s.program[i];
+    }
+
+    return (nameSum + courseSum) % MAX;
+}
+
+Student createStudent(char *name, char *course, int id, int year){
+    Student temp;
+    temp.studID = id;
+    temp.year = year;
+    strcpy(temp.studName,name);
+    strcpy(temp.program,course);
+
+    return temp;
+}
+
+int insert(HashTable *ht, Student s){
+    int start = getHash(s);
+    NodePtr* trav, temp;
+    for(trav = &ht->elems[start]; *trav != NULL && (*trav)->stud.studID < s.studID; trav = &(*trav)->link){}
+
+    if(*trav != NULL && (*trav)->stud.studID == s.studID){
+        return 0;
+    }
+
+    temp = (NodePtr)malloc(sizeof(struct node));
+    if(temp != NULL){
+        temp->stud = s;
+        temp->link = *trav;
+        *trav = temp;
+        ht->count++;
+        return 1;
+    }
+
+    return 0;
+}
+
+int delete(HashTable *ht, Student s){
+    int start = getHash(s);
+    NodePtr* trav, temp;
+    for(trav = &ht->elems[start]; *trav != NULL && (*trav)->stud.studID != s.studID; trav = &(*trav)->link){}
+
+    if(*trav != NULL){
+        temp = *trav;
+        *trav = temp->link;
+        free(temp);
+        ht->count--;
+        return 1;
+    }
+    return 0;
+}
+
+void visualizeTable(HashTable ht){
+    int i;
+    NodePtr trav;
+    for(i=0 ;i<MAX;i++){
+        printf("%d: ",i);
+        for(trav = ht.elems[i]; trav != NULL; trav=trav->link){
+            printf("%s ==> ", trav->stud.studName);
+        }
+        printf("NULL\n");
+    }
+}
