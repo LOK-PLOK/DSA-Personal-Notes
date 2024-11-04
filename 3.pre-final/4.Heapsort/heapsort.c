6afc5f977fca23fe718f074f4085136dacadd34b
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-11-05 01:18:31 +0800

    feat: heapsort function understood

--- 3.pre-final/4.Heapsort/heapsort.c
+++ 3.pre-final/4.Heapsort/heapsort.c
@@ -1,65 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define MAX 20 
+#define MAX 15
 
 typedef struct {
     int tree[MAX];
     int lastNdx;
 }POT;
 
-void initPot(POT* pq);
+void initPOT(POT* pq);
+void insert(POT* pq, int elem);
+void deleteMin(POT* pq);
+void populate(POT* pq,int data[],int size);
+void heapify(POT* pq, int parent);
 void display(POT pq);
 void displayV2(POT pq);
-void insert(POT* pq,int elem);
-void populate(POT* pq,int data[],int size);
-void heapify(POT* pq, int index);
-int deleteMin(POT* pq);
-
-void heapsort(POT* pq,int size);
+void heapsort(POT* pq,int lastNdx);
 
 int main(){
     POT pq;
-    initPot(&pq);
-    display(pq);
+    initPOT(&pq);
 
     int data[] = {6,10,30,1,8,55,30,15,100,25};
     populate(&pq,data,sizeof(data)/sizeof(data[0]));
     display(pq);
-    displayV2(pq);
 
+
+    //heapsort
+    printf("HeapSort: ");
     heapsort(&pq,pq.lastNdx);
     displayV2(pq);
 
+    //deleteMin
+    printf("DeleteMin:\n");
+    deleteMin(&pq);
+    display(pq);
+
+    
+    return 0;
 }
 
-
-void initPot(POT* pq){
+void initPOT(POT* pq){
     pq->lastNdx = -1;
 }
 
-void insert(POT* pq,int elem){
+void insert(POT* pq, int elem){
     if(pq->lastNdx != MAX){
-        pq->tree[++pq->lastNdx] = elem;
+        pq->tree[++pq->lastNdx]= elem;
 
-        for(int idx = pq->lastNdx; idx>=0 && pq->tree[idx] < pq->tree[(idx-1)/2]; idx =(idx-1)/2 ){
+        for(int idx = pq->lastNdx; idx>=0 && pq->tree[idx] < pq->tree[(idx-1)/2]; idx = (idx-1)/2){
             int temp = pq->tree[idx];
             pq->tree[idx] = pq->tree[(idx-1)/2];
             pq->tree[(idx-1)/2] = temp;
+
         }
     }
 }
 
+void deleteMin(POT* pq){
+    if(pq->lastNdx != -1){
+        int deleteElem = pq->tree[0];
+        pq->tree[0] = pq->tree[pq->lastNdx--];
+        heapify(pq,0);
+    }
+}
+
 void populate(POT* pq,int data[],int size){
-    for(int i = 0; i < size; i++){
+    for(int i = 0; i<size; i++){
         insert(pq,data[i]);
     }
 }
 
-void heapify(POT* pq, int index){
-    int small = index;
-    int LC = index*2+1;
-    int RC = index*2+2;
+void heapify(POT* pq, int parent){
+    int small = parent;
+    int LC = parent*2+1;
+    int RC = parent*2+2;
 
     if(LC <= pq->lastNdx && pq->tree[LC] < pq->tree[small]){
         small = LC;
@@ -69,51 +84,21 @@ void heapify(POT* pq, int index){
         small = RC;
     }
 
-    if(small != index){
+    if(small != parent){
         int temp = pq->tree[small];
-        pq->tree[small] = pq->tree[index];
-        pq->tree[index] = temp;
+        pq->tree[small] = pq->tree[parent];
+        pq->tree[parent] = temp;
         heapify(pq,small);
     }
-}
-
-int deleteMin(POT* pq){
-    if(pq->lastNdx != -1){
-        int temp = pq->tree[pq->lastNdx];
-        pq->tree[0] = pq->tree[pq->lastNdx];
-        pq->lastNdx--;
-        heapify(pq,0);
-        return temp;
-    }
-}
-
-void heapsort(POT* pq, int size) {
-    int lastIndex = pq->lastNdx; // remembers the last index
-
-    // Extract elements from the heap one by one
-    for (int i = lastIndex; i > 0; i--) {
-        // Move current root to end
-        int temp = pq->tree[0];
-        pq->tree[0] = pq->tree[i];
-        pq->tree[i] = temp;
-
-        // Reduce the heap size
-        pq->lastNdx--;
-
-        // Heapify the root element to maintain the max-heap property
-        heapify(pq, 0);
-    }
-
-    // Restore the original lastNdx value
-    pq->lastNdx = size - 1;
 }
 
 void display(POT pq){
     if(pq.lastNdx == -1){
         printf("EMPTY");
     }else{
-        printf("%-20s%-20s\n","Priority","Index");
-        for(int i=0; i <= pq.lastNdx; i++){
+        int i;
+        printf("%-20s%-20s\n","index","Priority");
+        for(i=0;i<=pq.lastNdx;i++){
             printf("%-20d%-20d\n",i,pq.tree[i]);
         }
     }
@@ -125,10 +110,27 @@ void displayV2(POT pq){
     if(pq.lastNdx == -1){
         printf("EMPTY");
     }else{
-        for(int i =0; i<=pq.lastNdx; i++){
+        int i;
+        for(i=0;i<=pq.lastNdx;i++){
             printf("%d ",pq.tree[i]);
         }
     }
 
     printf("\n\n");
+}
+
+void heapsort(POT* pq,int lastNdx){
+    int lastIndex = lastNdx;
+
+    for(int i = lastIndex; i>0; i--){
+        int temp = pq->tree[0];
+        pq->tree[0] = pq->tree[i];
+        pq->tree[i] = temp;
+
+        pq->lastNdx--;
+
+        heapify(pq,0);
+    }
+
+    pq->lastNdx = lastIndex;
 }
