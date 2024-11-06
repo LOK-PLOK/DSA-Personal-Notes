a32665bdf313c8b8b990fc370c878dcb99f6dcc2
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-11-06 22:36:07 +0800

    feat: Parent-pointer review

--- 3.pre-final/1.Tree/parent-pointer.c
+++ 3.pre-final/1.Tree/parent-pointer.c
@@ -1,66 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define MAX 10
+#define ROOT  -1
+#define NO_CHILD  -2
 
-typedef int Tree[MAX];
+typedef int tree[MAX];
 
-Tree* initTree();
-
-void display(Tree T);
-void displayParents(int node,Tree T);
-void insert(int elem,int parent,Tree* T);
+tree* initTree();
+void insert(int elem,int parent,tree* T); // elemenent to insert, the parent, reference to the tree
+void display(tree T);
+int PARENT(int n, tree T);
+int LEFTMOST_CHILD(int n, tree T);
+int RIGHT_SIBLING(int n, tree T);
+int Root(tree T);
+void MAKENULL(tree* T);
 
 int main(){
-    Tree* T = initTree();
-
-    insert(3,-1,T);
-    insert(5,3,T);
-    insert(6,3,T);
-    insert(4,6,T);
-
+    // system("cls");
+    system("clear");
+    tree* T = initTree();
+    insert(5,ROOT,T);
+    insert(2,5,T);
+    insert(4,5,T);
+    insert(0,5,T);
+    insert(7,2,T);
+    insert(1,2,T);
+    insert(8,0,T);
+    insert(6,1,T);
+    
+    printf("Initial tree:\n");
     display(*T);
-    displayParents(4,*T);
-    free(T);
+
+    printf("\nTesting PARENT function:\n");
+    for (int i = 0; i < MAX; i++) {
+        printf("PARENT(%d) = %d\n", i, PARENT(i, *T));
+    }
+
+    printf("\nTesting LEFTMOST_CHILD function:\n");
+    for (int i = 0; i < MAX; i++) {
+        printf("LEFTMOST_CHILD(%d) = %d\n", i, LEFTMOST_CHILD(i, *T));
+    }
+
+    printf("\nTesting RIGHT_SIBLING function:\n");
+    for (int i = 0; i < MAX; i++) {
+        printf("RIGHT_SIBLING(%d) = %d\n", i, RIGHT_SIBLING(i, *T));
+    }
+
+    printf("\nTesting Root function:\n");
+    printf("Root = %d\n", Root(*T));
+
+    printf("\nTesting MAKENULL function:\n");
+    MAKENULL(T);
+    display(*T);
+
+    return 0;
 }
 
-Tree* initTree(){
-    Tree* temp = (Tree*)malloc(sizeof(Tree));
+tree* initTree(){
+    tree* temp = (tree*)malloc(sizeof(tree));
     if(temp != NULL){
         int i;
-        for(i = 0; i <MAX; i++){
-            (*temp)[i] = -1;
+        for(i=0;i<MAX;i++){
+            (*temp)[i] = NO_CHILD;
         }
-    }else{
-        printf("Allocation Failed\n\n");
     }
 
-    return temp;    
+    return temp;
 }
 
-void insert(int elem,int parent,Tree* T){
-    if(elem < MAX && elem >-1 && parent < MAX && parent > -1){
-        (*T)[elem] = parent;
-    }else{
-        printf("INVALID\n\n");
+void insert(int elem,int parent,tree* T){
+    if(elem < MAX && elem>=0 && parent >=-1 && parent < MAX){
+        if((*T)[elem] == NO_CHILD){
+            (*T)[elem] = parent;
+        }else{
+            printf("Parent already in place\n\n");
+        }
     }
 }
 
-void display(Tree T){
+void display(tree T){
     int i;
-    printf("Tree:\n");
-    for(i=0; i< MAX; i++){
-        printf("%d: %d\n",i,T[i]);
+    printf("%-10s%-10s\n","child","parent");
+    for(i=0;i<MAX;i++){
+        printf("%-10d%-10d\n",i,T[i]);
     }
-    printf("\n");
 }
 
-void displayParents(int node,Tree T){
-    if(node < MAX && node >-1 && T[node] != -1){
-        int i;
-        for(i=node; T[i] != -1; i = T[i]){
-            printf("%d ", i);
+int PARENT(int n, tree T){
+    return(T[n] != -2)? T[n]:-2;
+}
+
+int LEFTMOST_CHILD(int n, tree T){
+    int i;
+    for(i = 0; i <MAX && n != T[i]; i++){}
+
+    return (i< MAX)? i:-2;
+}
+
+int RIGHT_SIBLING(int n, tree T){
+    int i;
+    int r = -2;
+    int p = T[n];
+    int ctr = 0;
+
+    for(i = 0;i<MAX;i++){
+        if(T[i] == p && i>n && ctr == 0){
+            r = i;
+            ctr++;
         }
-        printf("%5d",i);
-        printf("\n");
+    } 
+
+    return r;
+}
+
+int Root(tree T){
+    int i;
+    for(i=0;i<MAX && T[i] != -1;i++){}
+
+    return (i<MAX)? i:-2;
+}
+
+void MAKENULL(tree* T){
+    int i; 
+    for(i=0;i<MAX;i++){
+        (*T)[i] = NO_CHILD;
     }
 }
