e91d9a7fc9f0a10cf4d32e5e8bbfd707ccae11ce
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-11-10 03:26:20 +0800

    feat: fix logic error and improve code logic

--- 3.pre-final/1.Tree/child-pointer.c
+++ 3.pre-final/1.Tree/child-pointer.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX 10
 
 typedef struct node{
     int data;
     struct node* link;
-}*List;
+}*Node;
 
-typedef struct { // similar to a open-hashing data structure
-    List ptr[MAX];
+typedef struct{
+    Node List[MAX];
     int root;
 }Tree;
 
@@ -17,7 +18,7 @@ void insert(Tree* tree, int elem, int parent);
 void display(Tree tree);
 int PARENT(int n, Tree T);
 int LEFTMOST_CHILD(int n, Tree T);
-int RIGHTMOST_SIBLING(int n, Tree T);
+int RIGHT_SIBLING(int n, Tree T);
 int ROOT(Tree T);
 int MAKENULL(Tree* T);
 
@@ -50,7 +51,7 @@ int main(){
 
     printf("\nTesting RIGHTMOST_SIBLING function:\n");
     for (int i = 0; i < MAX; i++) {
-        printf("RIGHTMOST_SIBLING(%d) = %d\n", i, RIGHTMOST_SIBLING(i, tree));
+        printf("RIGHTMOST_SIBLING(%d) = %d\n", i, RIGHT_SIBLING(i, tree));
     }
 
     printf("\nTesting ROOT function:\n");
@@ -61,21 +62,22 @@ int main(){
     display(tree);
 
     return 0;
+
 }
 
 void init(Tree* tree, int root){
     tree->root = root;
     int i;
     for(i=0;i<MAX;i++){
-        tree->ptr[i] = NULL;
+        tree->List[i] = NULL;
     }
 }
 
 void insert(Tree* tree, int elem, int parent){
-    if(parent >=0 && parent < MAX){
-        List *trav,temp;
-        for(trav = &tree->ptr[parent]; *trav != NULL; trav = &(*trav)->link){}
-        temp = (List)malloc(sizeof(struct node));
+    if(parent < MAX && parent >= 0){
+        Node* trav,temp;
+        for(trav = &tree->List[parent]; *trav != NULL;trav= &(*trav)->link){}
+        temp = (Node)malloc(sizeof(struct node));
         if(temp != NULL){
             temp->data = elem;
             temp->link = *trav;
@@ -86,52 +88,54 @@ void insert(Tree* tree, int elem, int parent){
 
 void display(Tree tree){
     int i;
-    List trav;
-
-    for(i = 0; i<MAX;i++){
+    Node trav;
+    
+    for(i=0;i<MAX;i++){
         printf("%d: ",i);
-        for(trav = tree.ptr[i]; trav != NULL;trav = trav->link){
-            printf("%d --> ", trav->data);
+        for(trav = tree.List[i];trav != NULL; trav = trav->link){
+            printf("%d --> ",trav->data);
         }
         printf("NULL\n");
     }
 }
 
 int PARENT(int n, Tree T){
-    List trav;
-    int i, p = -2;
-    for(i = 0; i < MAX; i++){
-        for(trav = T.ptr[i]; trav != NULL;trav = trav->link){
-            if(trav->data == n){
-                p = i;
+    Node trav;
+    int i,parent = -2;
+    if(n != T.root){
+        for(i=0;i<MAX;i++){
+            for(trav = T.List[i];trav != NULL; trav = trav->link){
+                if(trav->data == n){
+                    parent = i;
+                }
+            }
+        }
+    }else{
+        parent = -1;
+    }
+    return parent;
+}
+
+// n is the parent
+int LEFTMOST_CHILD(int n, Tree T){
+    return (n < MAX && T.List[n] != NULL)? T.List[n]->data:-2;
+}
+
+// n is the child
+int RIGHT_SIBLING(int n, Tree T) {
+    int parent = PARENT(n, T); // Get the parent of the node
+    int rightmostSibling = -2; // Initialize rightmost sibling to NO_CHILD
+
+    if (parent >=0 ) { // Check if the node has a parent
+        Node trav;
+        for (trav = T.List[parent]; trav != NULL; trav = trav->link) {
+            if (trav->data != n) {
+                rightmostSibling = trav->data; // Update rightmost sibling
             }
         }
     }
 
-    return p;
-}
-
-int LEFTMOST_CHILD(int n, Tree T){
-    return (n < MAX && T.ptr[n] != NULL)? T.ptr[n]->data: -2;
-}
-
-int RIGHTMOST_SIBLING(int n, Tree T){
-    int parent = PARENT(n, T);
-    if (parent == -2) {
-        return -2; 
-    }
-
-    List trav = T.ptr[parent];
-    int rightmostSibling = -2;
-
-    while (trav != NULL) {
-        if (trav->data != n) {
-            rightmostSibling = trav->data;
-        }
-        trav = trav->link;
-    }
-
-    return rightmostSibling;
+    return rightmostSibling; // Return the rightmost sibling
 }
 
 int ROOT(Tree T){
@@ -141,6 +145,7 @@ int ROOT(Tree T){
 int MAKENULL(Tree* T){
     int i;
     for(i=0;i<MAX;i++){
-        T->ptr[i] = NULL;
+        T->List[i] = NULL;
     }
+    T->root = -1;
 }
