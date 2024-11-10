7122c6419edf7e85cc4efdc59f6f76a94e3b2960
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-11-10 08:14:57 +0800

    feat: finally fixed the heapsort logic

--- 3.pre-final/4.Heapsort/heapsort_array.c
+++ 3.pre-final/4.Heapsort/heapsort_array.c
@@ -3,13 +3,15 @@
 #define MAX 10
 
 void display(int data[], int size);
-void heapifyAll(int data[], int size);
+void heapifyAllMin(int data[], int size);
+void heapifyAllMax(int data[], int size);
 void heapifySubtreeMin(int data[], int lastNdx, int parent);
 void heapifySubtreeMax(int data[], int lastNdx, int parent);
 void heapsortMin(int data[], int size);
 void heapsortMax(int data[], int size);
 
 int main() {
+    system("clear");
     int data[] = {61, 10, 30, 1, 8, 25, 30, 0, 100, 55};
     int size = sizeof(data) / sizeof(data[0]);
     display(data, size);
@@ -28,90 +30,109 @@ int main() {
     return 0;
 }
 
-void heapifyAll(int data[], int size) {
-    int lastNdx = size - 1;
-    for (int i = (lastNdx - 1) / 2; i >= 0; i--) {
-        heapifySubtreeMax(data, lastNdx, i);
+void heapifyAllMin(int data[], int size){
+    int lastNdx = size-1;
+    int i;
+    for(i=(lastNdx-1)/2; i>=0; i--){
+        heapifySubtreeMin(data,lastNdx,i);
     }
 }
 
-void heapifySubtreeMin(int data[], int lastNdx, int parent) {
-    int small, LC, RC, flag = 0;
-    while (!flag) {
+void heapifyAllMax(int data[], int size){
+    int lastNdx = size-1;
+    int i;
+    for(i=(lastNdx-1)/2; i>=0; i--){
+        heapifySubtreeMax(data,lastNdx,i);
+    }
+}
+
+void heapifySubtreeMin(int data[], int lastNdx, int parent){
+    int small,LC,RC,flag=0;
+
+    while(!flag){
         small = parent;
-        LC = parent * 2 + 1;
+        LC = parent*2+1;
         RC = LC + 1;
 
-        if (LC <= lastNdx && data[LC] < data[small]) {
+        if(LC <= lastNdx && data[LC] < data[small]){
             small = LC;
         }
 
-        if (RC <= lastNdx && data[RC] < data[small]) {
+        if(RC <= lastNdx && data[RC] < data[small]){
             small = RC;
         }
 
-        if (small != parent) {
+        if(small != parent){
             int temp = data[small];
             data[small] = data[parent];
             data[parent] = temp;
             parent = small;
-        } else {
+        }else{
             flag = 1;
         }
     }
 }
 
-void heapifySubtreeMax(int data[], int lastNdx, int parent) {
-    int large, LC, RC, flag = 0;
-    while (!flag) {
+void heapifySubtreeMax(int data[], int lastNdx, int parent){
+    int large,LC,RC,flag=0;
+
+    while(!flag){
         large = parent;
-        LC = parent * 2 + 1;
+        LC = parent*2+1;
         RC = LC + 1;
 
-        if (LC <= lastNdx && data[LC] > data[large]) {
+        if(LC <= lastNdx && data[LC] > data[large]){
             large = LC;
         }
 
-        if (RC <= lastNdx && data[RC] > data[large]) {
+        if(RC <= lastNdx && data[RC] > data[large]){
             large = RC;
         }
 
-        if (large != parent) {
+        if(large != parent){
             int temp = data[large];
             data[large] = data[parent];
             data[parent] = temp;
             parent = large;
-        } else {
+        }else{
             flag = 1;
         }
     }
 }
 
-void heapsortMin(int data[], int size) {
-    int originalLastNdx = size - 1;
-    heapifyAll(data, size);
+void heapsortMin(int data[], int size){
+    int i;
+    int lastNdx = size-1;
 
-    for (int i = originalLastNdx; i > 0; i--) {
+    for(i=(lastNdx-1)/2;i>=0;i--){
+        heapifyAllMin(data,size);
+    }
+
+    for(i=lastNdx;i>0;i--){
         int temp = data[0];
         data[0] = data[i];
         data[i] = temp;
 
-        originalLastNdx--;
-        heapifySubtreeMin(data, originalLastNdx, 0);
+        lastNdx--;
+        heapifySubtreeMin(data,lastNdx,0);
     }
 }
 
-void heapsortMax(int data[], int size) {
-    int originalLastNdx = size - 1;
-    heapifyAll(data, size);
+void heapsortMax(int data[], int size){
+    int i;
+    int lastNdx = size-1;
 
-    for (int i = originalLastNdx; i > 0; i--) {
+    for(i=(lastNdx-1)/2;i>=0;i--){
+        heapifyAllMax(data,size);
+    }
+
+    for(i=lastNdx;i>0;i--){
         int temp = data[0];
         data[0] = data[i];
         data[i] = temp;
 
-        originalLastNdx--;
-        heapifySubtreeMax(data, originalLastNdx, 0);
+        lastNdx--;
+        heapifySubtreeMax(data,lastNdx,0);
     }
 }
 
