8377dbb93176eecafafe38158f499be0be972126
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-11-10 15:05:10 +0800

    feat: heapsort POT fixed

--- 3.pre-final/4.Heapsort/heapsort_priorityQueue.c
+++ 3.pre-final/4.Heapsort/heapsort_priorityQueue.c
@@ -13,11 +13,14 @@ void display(POT heap);
 void displayV2(POT heap);
 void insert(POT* heap, int elem);
 void populate(POT* heap, int data[], int size);
-void heapiftyAll(POT* heap);
-void heapify(POT* heap, int parent);
-void heapifyV2Min(POT* heap, int parent);
-void heapifyV2Max(POT* heap, int parent);
+void heapiftyAllMin(POT* heap);
+void heapiftyAllMax(POT* heap);
+void heapifMinRec(POT* heap, int parent);
+void heapifMaxRec(POT* heap, int parent);
+void heapifyMin(POT* heap, int parent);
+void heapifyMax(POT* heap, int parent);
 int deleteMin(POT* heap);
+int deleteMax(POT* heap);
 void heapsortMin(POT* heap);
 void heapsortMax(POT* heap);
 
@@ -47,11 +50,11 @@ int main() {
     return 0;
 }
 
-void init(POT* heap) {
+void init(POT* heap){
     heap->lastNdx = -1;
 }
 
-void display(POT heap) {
+void display(POT heap){
     if (heap.lastNdx == -1) {
         printf("EMPTY");
     } else {
@@ -75,149 +78,191 @@ void displayV2(POT heap){
         }
         printf("\n\n");
     }
-
 }
 
-void insert(POT* heap, int elem) {
-    if (heap->lastNdx < MAX - 1) {
+void insert(POT* heap, int elem){
+    if(heap->lastNdx != MAX-1){
         heap->tree[++heap->lastNdx] = elem;
-
-        int idx;
-        for (idx = heap->lastNdx; idx > 0 && heap->tree[idx] < heap->tree[(idx - 1) / 2]; idx = (idx - 1) / 2) {
-            int temp = heap->tree[idx];
-            heap->tree[idx] = heap->tree[(idx - 1) / 2];
-            heap->tree[(idx - 1) / 2] = temp;
-        }
+    }else{
+        printf("POT FULL\n\n");
     }
 }
 
-void populate(POT* heap, int data[], int size) {
+void populate(POT* heap, int data[], int size){
     int i;
-    for (i = 0; i < size; i++) {
-        insert(heap, data[i]);
+    for(i=0;i<size;i++){
+        insert(heap,data[i]);
     }
 }
 
-void heapiftyAll(POT* heap) {
+void heapiftyAllMin(POT* heap){
+    int i;
     int lastNdx = heap->lastNdx;
-    int i;
-    for (i = (lastNdx - 1) / 2; i >= 0; i--) {
-        heapify(heap, i);
+    for(i = (lastNdx-1)/2;i>=0;i--){
+        heapifyMin(heap,i);
     }
 }
 
-void heapify(POT* heap, int parent) {
+void heapiftyAllMax(POT* heap){
+    int i;
+    int lastNdx = heap->lastNdx;
+    for(i = (lastNdx-1)/2;i>=0;i--){
+        heapifyMin(heap,i);
+    }
+}
+
+void heapifMinRec(POT* heap, int parent){
+    int i;
     int small = parent;
     int LC = parent * 2 + 1;
     int RC = LC + 1;
 
-    if (LC <= heap->lastNdx && heap->tree[LC] < heap->tree[small]) {
+    if(LC <= heap->lastNdx && heap->tree[LC] < heap->tree[small]){
         small = LC;
     }
 
-    if (RC <= heap->lastNdx && heap->tree[RC] < heap->tree[small]) {
+    if(RC <= heap->lastNdx && heap->tree[RC] < heap->tree[small]){
         small = RC;
     }
 
-    if (small != parent) {
+    if(small != parent){
         int temp = heap->tree[small];
         heap->tree[small] = heap->tree[parent];
         heap->tree[parent] = temp;
-        heapify(heap, small);
+        heapifMinRec(heap,small);
     }
 }
 
-void heapifyV2Min(POT* heap, int parent) {
-    int small, LC, RC, flag = 0;
+void heapifMaxRec(POT* heap, int parent){
+    int i;
+    int large = parent;
+    int LC = parent * 2 + 1;
+    int RC = LC + 1;
 
-    while (!flag) {
+    if(LC <= heap->lastNdx && heap->tree[LC] > heap->tree[large]){
+        large = LC;
+    }
+
+    if(RC <= heap->lastNdx && heap->tree[RC] > heap->tree[large]){
+        large = RC;
+    }
+
+    if(large != parent){
+        int temp = heap->tree[large];
+        heap->tree[large] = heap->tree[parent];
+        heap->tree[parent] = temp;
+        heapifMinRec(heap,large);
+    }
+}
+
+void heapifyMin(POT* heap, int parent){
+    int small,LC,RC,flag = 0;
+
+    while(!flag){
         small = parent;
         LC = parent * 2 + 1;
         RC = LC + 1;
 
-        if (LC <= heap->lastNdx && heap->tree[LC] < heap->tree[small]) {
-            small = LC;
+        if(LC <= heap->lastNdx && heap->tree[LC] < heap->tree[small]){
+        small = LC;
         }
 
-        if (RC <= heap->lastNdx && heap->tree[RC] < heap->tree[small]) {
+        if(RC <= heap->lastNdx && heap->tree[RC] < heap->tree[small]){
             small = RC;
         }
 
-        if (small != parent) {
+        if(small != parent){
             int temp = heap->tree[small];
             heap->tree[small] = heap->tree[parent];
             heap->tree[parent] = temp;
             parent = small;
-        } else {
+        }else{
             flag = 1;
         }
     }
 }
-void heapifyV2Max(POT* heap, int parent) {
-    int large, LC, RC, flag = 0;
 
-    while (!flag) {
+void heapifyMax(POT* heap, int parent){
+    int large,LC,RC,flag = 0;
+
+    while(!flag){
         large = parent;
         LC = parent * 2 + 1;
         RC = LC + 1;
 
-        if (LC <= heap->lastNdx && heap->tree[LC] > heap->tree[large]) {
-            large = LC;
+        if(LC <= heap->lastNdx && heap->tree[LC] > heap->tree[large]){
+        large = LC;
         }
 
-        if (RC <= heap->lastNdx && heap->tree[RC] > heap->tree[large]) {
+        if(RC <= heap->lastNdx && heap->tree[RC] > heap->tree[large]){
             large = RC;
         }
 
-        if (large != parent) {
+        if(large != parent){
             int temp = heap->tree[large];
             heap->tree[large] = heap->tree[parent];
             heap->tree[parent] = temp;
             parent = large;
-        } else {
+        }else{
             flag = 1;
         }
     }
 }
 
-int deleteMin(POT* heap) {
-    if (heap->lastNdx != -1) {
-        int deleteElem = heap->tree[0];
+int deleteMin(POT* heap){
+    if(heap->lastNdx != -1){
+        int deletedElem = heap->tree[0];
         heap->tree[0] = heap->tree[heap->lastNdx];
-        heap->tree[heap->lastNdx--] = deleteElem;
+        heap->tree[heap->lastNdx --] = deletedElem;
 
-        heapify(heap, 0);
-        return deleteElem;
+        heapifyMin(heap,0);
+        return deletedElem;
     }
-    return -1; // Return a value to indicate the heap was empty
 }
 
-void heapsortMin(POT* heap) {
-    int originalLastNdx = heap->lastNdx;
+int deleteMax(POT* heap){
+    if(heap->lastNdx != -1){
+        int deletedElem = heap->tree[0];
+        heap->tree[0] = heap->tree[heap->lastNdx];
+        heap->tree[heap->lastNdx --] = deletedElem;
 
-    for (int i = heap->lastNdx; i > 0; i--) {
+        heapifyMax(heap,0);
+        return deletedElem;
+    }
+}
+
+void heapsortMin(POT* heap){
+    int i;
+    int lastNdx = heap->lastNdx;
+    for(i = (lastNdx-1)/2;i>=0;i--){
+        heapifyMin(heap,i);
+    }
+    for(i=heap->lastNdx; i>0;i--){
         int temp = heap->tree[0];
         heap->tree[0] = heap->tree[i];
         heap->tree[i] = temp;
 
-        heap->lastNdx--;
-        heapifyV2Min(heap, 0);
+        heap->lastNdx --;
+        heapifyMin(heap,0);
     }
 
-    heap->lastNdx = originalLastNdx;
+    heap->lastNdx = lastNdx;
 }
 
-void heapsortMax(POT* heap) {
-    int originalLastNdx = heap->lastNdx;
-
-    for (int i = heap->lastNdx; i > 0; i--) {
+void heapsortMax(POT* heap){
+    int i;
+    int lastNdx = heap->lastNdx;
+    for(i = (lastNdx-1)/2;i>=0;i--){
+        heapifyMax(heap,i);
+    }
+    for(i=heap->lastNdx; i>0;i--){
         int temp = heap->tree[0];
         heap->tree[0] = heap->tree[i];
         heap->tree[i] = temp;
 
-        heap->lastNdx--;
-        heapifyV2Max(heap, 0);
+        heap->lastNdx --;
+        heapifyMax(heap,0);
     }
 
-    heap->lastNdx = originalLastNdx;
+    heap->lastNdx = lastNdx;
 }
