044f3d3eeda79749a7f9803797b51ab17c6a15d2
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-12-01 00:06:08 +0800

    feat: AdjList representation in C

---
+++ 4.final/AdjList.c
@@ -0,0 +1,73 @@
+#include "strucDef.h"
+
+void initLabelAdjList(AdjListLL* List);
+void displayAdjList(AdjListLL List);
+void insertToAdjList(AdjListLL* List,int data[][MAXV],int size);
+void insert(AdjListLL* List,int row,int col);
+
+int main(){
+    AdjListLL List;
+    initLabelAdjList(&List);
+
+    int graphUndirected[MAXV][MAXV] = {
+        {x,1,0,1,0,1},
+        {1,x,0,0,1,0},
+        {0,0,x,1,1,0},
+        {1,0,1,x,0,1},
+        {0,1,1,0,x,0},
+        {1,0,0,1,0,x}
+    };
+
+    insertToAdjList(&List,graphUndirected,MAXV);
+    displayAdjList(List);
+
+    return 0;
+}
+
+void initLabelAdjList(AdjListLL* List){
+    int i;
+    for(i=0;i<MAXV;i++){
+        List->head[i] = NULL;
+    }
+    List->edgeCount = 0;
+}
+
+void insert(AdjListLL* List,int row,int col){
+    int i;
+    vert *trav,temp;
+    //loop 3
+    for(trav = &List->head[row]; *trav != NULL; trav = &(*trav)->next){}
+    temp = (vert)malloc(sizeof(struct nodeV));
+    if(temp != NULL){
+        temp->v = col;
+        temp->next = *trav;
+        *trav = temp;
+    }
+}
+
+void insertToAdjList(AdjListLL* List,int data[][MAXV],int size){
+    int row,col;
+    //loop 1
+    for(row = 0; row < size; row++){
+        //loop 2
+        for(col = 0; col < size; col++){
+            if(data[row][col] != 0 && data[row][col] != x){
+                insert(List,row,col);
+            }
+        }
+    }
+}
+
+void displayAdjList(AdjListLL List){
+    vert trav;
+    int i;
+    for(i = 0; i < MAXV; i ++){
+        printf("%d: ", i);
+        for(trav = List.head[i]; trav != NULL; trav = trav->next){
+            printf("%d --> ",trav->v);
+        }
+        printf("NULL\n");
+    }
+
+    puts("");
+}
