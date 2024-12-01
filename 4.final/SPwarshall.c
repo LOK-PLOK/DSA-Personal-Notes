265e6712f0d05a9288586b6738aff8fdadd80b0a
LOK-PLOK <157859793+LOK-PLOK@users.noreply.github.com>
2024-12-01 16:15:05 +0800

    feat: added Warshall's Algorithm in C

--- 4.final/SPwarshall.c
+++ 4.final/SPwarshall.c
@@ -1,19 +1,19 @@
 #include "strucDef.h"
 
-void displayLabelAdj(labelAdj_M G);
-void floyd(labelAdj_M A, labelAdj_M C);
+void displayLabelAdj(Adj_M G);
+void warshall(Adj_M A, Adj_M C);
 
 int main() {
-    labelAdj_M G = {
-        { INF, 10, INF, 30, 100 },
-        { INF, INF, 50, INF, INF },
-        { 20, INF, INF, INF, 10 },
-        { INF, INF, 20, INF, 60 },
-        { INF, INF, INF, INF, INF }
+    Adj_M G = {
+        { 0, 1, 0, 1, 1 },
+        { 0, 0, 1, 0, 0 },
+        { 1, 0, 0, 0, 1 },
+        { 0, 0, 1, 0, 1 },
+        { 0, 0, 0, 0, 0 }
     };
 
-    labelAdj_M A = {INF};
-    floyd(A,G);
+    Adj_M A = {0};
+    warshall(A,G);
     int i,j;
     puts("");
     for(i=0;i<MAXV;i++){
@@ -29,33 +29,31 @@ int main() {
     return 0;
 }
 
-void floyd(labelAdj_M A, labelAdj_M C){
+void warshall(Adj_M A, Adj_M C){
     int i,j,k;
-    // Copy the Labeled Adjacency Graph C to A
     for(i=0;i<MAXV;i++){
         for(j=0;j<MAXV;j++){
             A[i][j] = C[i][j];
         }
     }
-    // Set the Diagonal Line to 0
-    for(i=0;i<MAXV;i++){
-        A[i][i] = 0;
-    }
-    
-    // Floyd-Warshall algorithm to find shortest paths
+
+    /**
+     * Applies the Floyd-Warshall algorithm to find the transitive closure.
+     * Updates adjacency matrix A to reflect reachability of vertices.
+     * 1 indicates a path, 0 indicates no path.
+     */
     for(k=0;k<MAXV;k++){
         for(i=0;i<MAXV;i++){
             for(j=0;j<MAXV;j++){
-                // Update shortest path if a shorter path is found
-                if(A[i][k] + A[k][j] < A[i][j]){
-                    A[i][j] = A[i][k] + A[k][j];
+                if(A[i][j] == 0){
+                    A[i][j] = A[i][k] && A[k][j];
                 }
             }
         }
     }
 }
 
-void displayLabelAdj(labelAdj_M G) {
+void displayLabelAdj(Adj_M G) {
     int row, col;
     puts("");
     printf("    ");
